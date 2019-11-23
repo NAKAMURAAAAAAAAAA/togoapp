948f543ce34d3109138465c66908cf2664875365
Kan Nakamura <kan.nakamura0711@gmail.com>
2019-11-23 12:39:31 +0900

    initial commit

---
+++ togoapp/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  togoapp
+//
+//  Created by Kan Nakamura on 2019/11/23.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ togoapp/SignupViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SignupViewController.swift
+//  togoapp
+//
+//  Created by Kan Nakamura on 2019/11/23.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignupViewController: UIViewController,UITextFieldDelegate {
+    //emailのテキストフィールド
+    @IBOutlet weak var emailTextField: UITextField!
+    //passwordのテキストフィールド
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        emailTextField.delegate = self //デリゲートをセット（デリゲートって何ですか？）
+        passwordTextField.delegate = self //デリゲートをセット
+        passwordTextField.isSecureTextEntry = true // 文字を非表示に
+    }
+    //サインアップボタン
+    @IBAction func willSignup(_ sender: Any) {
+        //サインアップのための関数
+        signup()
+    }
+    //ログインボタン
+    @IBAction func willTransitionToLogin(_ sender: Any) {
+        transitionToLogin()
+    }
+    
+    //ログイン画面への遷移
+    func transitionToLogin() {
+        self.performSegue(withIdentifier: "toLogin", sender: self)
+    }
+    //ListViewControllerへの遷移
+    //まだstoryboardIDに"toView"つけてない
+    func transitionToView() {
+        self.performSegue(withIdentifier: "toView", sender: self)
+    }
+    //Returnキーを押すと、キーボードを隠す
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //Signupのためのメソッド
+    func signup() {
+        //emailTextFieldとpasswordTextFieldに文字がなければ、その後の処理をしない
+        guard let email = emailTextField.text else  { return }
+        guard let password = passwordTextField.text else { return }
+        //FIRAuth.auth()?.createUserWithEmailでサインアップ
+        //第一引数にEmail、第二引数にパスワード
+        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+            //エラーなしなら、認証完了
+            if error == nil{
+                        // エラーがない場合にはそのままログイン画面に飛び、ログインしてもらう
+                        self.transitionToLogin()
+                }else {
+
+                print("\(error?.localizedDescription)")
+            }
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
