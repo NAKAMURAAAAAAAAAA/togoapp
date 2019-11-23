dc3f4b95d5392012473725008ba92dfe00216bb0
Kan Nakamura <kan.nakamura0711@gmail.com>
2019-11-23 12:04:34 +0900

    Initial Commit

---
+++ togoapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  togoapp
+//
+//  Created by Kan Nakamura on 2019/11/23.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
